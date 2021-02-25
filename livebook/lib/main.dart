d818e23fdb409aed7dd13cba2cb7787149dbe24e
Franck Asket <franck@command-space.com>
2021-02-25 06:53:39 +0000

    Progress

---
+++ livebook/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:livebook/utils/ourTheme.dart';
+import 'package:livebook/screens/login/localwidgets/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: OurTheme().buildTheme(),
+      home: OurLogin(),
+    );
+  }
+}
+
